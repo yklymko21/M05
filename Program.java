d66f045c42553d5b0f3c072b72fb5142cfa01d49
Yaroslav Klymko <yklymko21@ilg.cat>
2022-10-17 12:40:02 +0200

    Ejercicio 1

--- Program.java
+++ Program.java
@@ -5,7 +5,7 @@
  * Program to calculate the area and the perimeter
  * of a circle knowing the radius in meter.
  *
- * Copyright 2020 Yaroslav Klymko <yklymko@ilg.cat>
+ * Copyright 2020 Joan Sèculi <jseculi@escoladeltreball.org>
  *
  * This is free software, licensed under the GNU General Public License v3.
  * See http://www.gnu.org/licenses/gpl.html for more information.
